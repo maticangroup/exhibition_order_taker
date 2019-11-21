bdce85a0d88caa8b0f034f7e1d44867814b20a90
Matican Matican <maticangroup@gmail.com>
2019-11-21 18:52:40 +0330

    Amirhossein

--- new-order.php
+++ new-order.php
@@ -37,69 +37,136 @@ if (isset($_REQUEST['q'])) {
     }
 }
 ?>
-    <h1>New order</h1>
-    <div class="order-section">
-        <div class="order-owner">
-            <?= $order->getCustomer()->getName() ?> <?= $order->getCustomer()->getFamily() ?>
-        </div>
-        <div class="order-create-date">
-            <?= $order->getCreateDate() ?>
 
-        </div>
-        <div class="order-status">
-            <?= $order->getOrderStatus() ?>
-        </div>
 
-    </div>
-    <div class="single-order-section">
-        <div class="search">
-            <form action="new-order.php">
-                <input type="hidden" value="<?= $_REQUEST['cid'] ?>" name="cid">
-                <input class="search-input" type="search" placeholder="search for product..."
-                       value="<?= (isset($_REQUEST['q'])) ? ($_REQUEST['q']) : "" ?>" name="q">
-            </form>
-        </div>
-        <div class="search-results">
-            <?php foreach ($products as $product) : ?>
-                <div class="result">
-                    <div class="product-serial">
-                        <?= $product->getSerial() ?>
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-md-4 col-sm-12 col-xs-12">
+            <div class="panel">
+                <!-- Mini Stats Panel Start -->
+                <div class="miniStats--panel text-white bg-blue">
+                    <div class="miniStats--header" data-overlay="0.1">
+                        <p class="miniStats--label text-blue bg-white">
+                            <span>New Order</span>
+                        </p>
                     </div>
-                    <div class="product-name">
-                        <?= $product->getName() ?>
 
+                    <div class="miniStats--body">
+                        <i class="miniStats--icon fa fa-user text-white"></i>
+
+                        <h3 class="miniStats--title h4 text-white">
+                            <?= $order->getCustomer()->getName() ?> <?= $order->getCustomer()->getFamily() ?></h3>
+                        <p class="miniStats--caption"><?= $order->getCreateDate() ?> </p>
+
+                        <p class="miniStats--num"<?= $order->getOrderStatus() ?></p>
                     </div>
-                    <div class="product-price">
-                        <?= $product->getPrice() ?>
+                </div>
+                <!-- Mini Stats Panel End -->
+            </div>
+        </div>
+        <div class="col-4"></div>
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="panel">
+                <div class="records--header">
+                    <div class="title fa-shopping-bag">
+                        <h3 class="h3">All Products</h3>
+                        <p>Found Total 1,330 Products</p>
                     </div>
-                    <div class="product-option">
-                        <form action="add-to-cart.php" method="get">
-                            <input type="hidden" name="cid" value="<?= $_REQUEST['cid'] ?>">
-                            <input type="hidden" name="product_id" value="<?= $product->getSerial() ?>">
-                            <input type="number" name="count">
-                            <input type="submit" value="add to cart">
+
+                    <div class="actions">
+                        <form action="new-order.php" class="search flex-wrap flex-md-nowrap">
+                            <input type="hidden" value="<?= $_REQUEST['cid'] ?>" name="cid">
+                            <input type="search" class="form-control" name="q"
+                                   value="<?= (isset($_REQUEST['q'])) ? ($_REQUEST['q']) : "" ?>"
+                                   placeholder="Product Name..."
+                                   required="">
+                            <button type="submit" class="btn btn-rounded"><i class="fa fa-search"></i></button>
                         </form>
                     </div>
                 </div>
-            <?php endforeach; ?>
+            </div>
         </div>
-        <div class="selected-products">
-            <?php foreach ($order->getOrderItems() as $orderItem) : ?>
-                <div class="product">
-                    <div class="order-product-serial">
-
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="panel">
+                <!-- Records List Start -->
+                <div class="records--list" data-title="Product Listing">
+                    <table id="recordsListView">
+                        <thead>
+                        <tr>
+                            <th>Serial</th>
+                            <th>Product Name</th>
+                            <th>Product Price</th>
+                            <th class="not-sortable">Product Count</th>
+                            <th class="not-sortable">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($products as $product) : ?>
+                            <tr>
+                                <td><?= $product->getSerial() ?></td>
+                                <td><?= $product->getName() ?></td>
+                                <td><?= $product->getPrice() ?></td>
+                                <form action="add-to-cart.php" method="get">
+                                    <input type="hidden" name="cid" value="<?= $_REQUEST['cid'] ?>">
+                                    <input type="hidden" name="product_id" value="<?= $product->getSerial() ?>">
+                                    <td>
+                                        <input type="number" class="form-control" name="count" required>
+                                    </td>
+                                    <td>
+                                        <input type="submit" class="btn btn-rounded btn-info" value="add to cart">
+                                    </td>
+                                </form>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- Records List End -->
+            </div>
+        </div>
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="panel">
+                <!-- Invoice Start -->
+                <div class="invoice">
+                    <div class="invoice--header">
+                        <div class="invoice--logo">
+                            <img src="http://bitaplastic.com/wp-content/themes/bita-fa/option-tree/assets/images/logo.png"
+                                 alt="Bita">
+                        </div>
                     </div>
-                    <div class="order-product-name">
 
-                    </div>
-                    <div class="order-product-price">
+                    <div class="invoice--order">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Product Serial</th>
+                                <th>Product Name</th>
+                                <th>Product Count</th>
+                                <th>Product Price</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($order->getOrderItems() as $orderItem) : ?>
+                                <tr>
+                                    <td>#12345678</td>
+                                    <td>20 Inch Philips LCD Black Color Monitor</td>
+                                    <td>12</td>
+                                    <td>$75.00</td>
+                                </tr>
+                            <?php endforeach; ?>
 
-                    </div>
-                    <div class="order-product-count">
-
+                            <tr>
+                                <td colspan="3"><strong>Subtotal</strong></td>
+                                <td>$1750.00</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            <?php endforeach; ?>
+                <!-- Invoice End -->
+            </div>
         </div>
     </div>
+
+
 <?php require "foot.php"; ?>
