3cc9acf521b3ac6cd9f351fab81addc155607a96
Matican Matican <maticangroup@gmail.com>
2019-11-21 17:24:48 +0330

    Amirhossein

--- head.php
+++ head.php
@@ -4,50 +4,158 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 ?>
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en" class="no-outlines">
 <head>
+
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="http://localhost:3000/assets/css/all.min.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- ==== Document Title ==== -->
+    <title>Dashboard - DAdmin</title>
+
+    <!-- ==== Document Meta ==== -->
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- ==== Favicon ==== -->
+    <link rel="icon" href="favicon.png" type="image/png">
+
+    <!-- ==== Google Font ==== -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CMontserrat:400,500">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="assets/css/perfect-scrollbar.min.css">
+    <link rel="stylesheet" href="assets/css/morris.min.css">
+    <link rel="stylesheet" href="assets/css/select2.min.css">
+    <link rel="stylesheet" href="assets/css/jquery-jvectormap.min.css">
+    <link rel="stylesheet" href="assets/css/horizontal-timeline.min.css">
+    <link rel="stylesheet" href="assets/css/weather-icons.min.css">
+    <link rel="stylesheet" href="assets/css/dropzone.min.css">
+    <link rel="stylesheet" href="assets/css/ion.rangeSlider.min.css">
+    <link rel="stylesheet" href="assets/css/ion.rangeSlider.skinFlat.min.css">
+    <link rel="stylesheet" href="assets/css/datatables.min.css">
+    <link rel="stylesheet" href="assets/css/fullcalendar.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Page Level Stylesheets -->
+
 </head>
 <body>
-<?php require "users.php"; ?>
-<?php require "actions.php"; ?>
-<?php require "entities.php"; ?>
-<?php if ($_SERVER['REQUEST_URI'] !== '/login.php'): ?>
-    <div class="dashboard-options">
-        <div class="option">
-            <a href="new-customer-order.php">
-                New Order
-            </a>
-        </div>
-        <div class="option">
-            <a href="orders-list.php">
-                Orders
-            </a>
-        </div>
-        <div class="option">
-            <a href="customers-list.php">
-                Customers
-            </a>
-        </div>
-        <div class="option">
-            <a href="products-list.php">
-                Products
-            </a>
-        </div>
-        <div class="option">
-            <a href="logout.php">
-                Logout
-            </a>
-        </div>
-    </div>
 
-<?php endif; ?>
+<!-- Wrapper Start -->
+<div class="wrapper">
+    <!-- Navbar Start -->
+    <header class="navbar navbar-fixed">
+        <!-- Navbar Header Start -->
+        <div class="navbar--header">
+            <!-- Logo Start -->
+            <a href="index.html" class="logo">
+                <img src="assets/img/logo.png" alt="">
+            </a>
+            <!-- Logo End -->
+
+            <!-- Sidebar Toggle Button Start -->
+            <a href="#" class="navbar--btn" data-toggle="sidebar" title="Toggle Sidebar">
+                <i class="fa fa-bars"></i>
+            </a>
+            <!-- Sidebar Toggle Button End -->
+        </div>
+        <!-- Navbar Header End -->
+
+        <!-- Sidebar Toggle Button Start -->
+        <a href="#" class="navbar--btn" data-toggle="sidebar" title="Toggle Sidebar">
+            <i class="fa fa-bars"></i>
+        </a>
+        <!-- Sidebar Toggle Button End -->
+
+        <div class="navbar--nav ml-auto">
+            <ul class="nav">
+
+
+                <?php if ($_SERVER['REQUEST_URI'] !== '/login.php'): ?>
+                    <!-- Nav User Start -->
+                    <li class="nav-item dropdown nav--user">
+                        <a href="#" class="nav-link">
+                            <span>Henry Foster</span>
+                        </a>
+                    </li>
+                    <!-- Nav User End -->
+                <?php endif; ?>
+            </ul>
+        </div>
+    </header>
+    <!-- Navbar End -->
+
+    <!-- Sidebar Start -->
+    <aside class="sidebar" data-trigger="scrollbar">
+        <?php require "users.php"; ?>
+        <?php require "actions.php"; ?>
+        <?php require "entities.php"; ?>
+        <?php if ($_SERVER['REQUEST_URI'] !== '/login.php'): ?>
+
+            <!-- Sidebar Navigation Start -->
+            <div class="sidebar--nav">
+                <ul>
+                    <li>
+                        <ul>
+                            <li class="active">
+                                <a href="new-customer-order.php">
+                                    <i class="fa fa-home"></i>
+                                    <span>New Order</span>
+                                </a>
+                            </li>
+
+                            <li class="active">
+                                <a href="orders-list.php">
+                                    <i class="fa fa-home"></i>
+                                    <span>Orders</span>
+                                </a>
+                            </li>
+
+                            <li class="active">
+                                <a href="customers-list.php">
+                                    <i class="fa fa-home"></i>
+                                    <span>Customers</span>
+                                </a>
+                            </li>
+
+                            <li class="active">
+                                <a href="products-list.php">
+                                    <i class="fa fa-home"></i>
+                                    <span>Products</span>
+                                </a>
+                            </li>
+
+                            <li class="active">
+                                <a href="logout.php">
+                                    <i class="fa fa-home"></i>
+                                    <span>Logout</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- Sidebar Navigation End -->
+
+
+        <?php endif; ?>
+
+    </aside>
+    <!-- Sidebar End -->
+
+    <!-- Main Container Start -->
+    <main class="main--container">
+
+
 
 
 
--- login.php
+++ login.php
@@ -1,34 +1,71 @@
 <?php require "head.php"; ?>
-<div class="login-form">
-    <form action="login.php">
-        <div class="login-form-element">
-            Welcome to the order taking platform
-        </div>
-        <div class="login-form-element">
-            <div class="element-title">
-                Username
-            </div>
-            <div class="element-input">
-                <input name="username" type="text">
-            </div>
-        </div>
-        <div class="login-form-element">
 
-            <div class="element-title">
-                Password
-            </div>
-            <div class="element-input">
-                <input name="password" type="password">
+
+<div class="row gutter-20 mt-5">
+    <div class="col-3"></div>
+    <div class="col-md-6">
+        <!-- Panel Start -->
+        <div class="panel">
+            <div class="panel-heading">
+                <h3 class="panel-title">Login Form</h3>
             </div>
 
+            <form action="login.php" class="panel-content">
+                <div class="form-group">
+                    <label>
+                        <span class="label-text">Username</span>
+                        <input type="text" name="username" placeholder="Enter Your Mobile Number..." class="form-control">
+                    </label>
+                </div>
+
+                <div class="form-group">
+                    <label>
+                        <span class="label-text">Password</span>
+                        <input type="password" name="password" placeholder="Enter Your Password..." class="form-control">
+                    </label>
+                </div>
+
+                <input type="submit" value="Submit" class="btn btn-sm btn-rounded btn-success">
+            </form>
         </div>
-        <div class="login-form-element submit">
-            <div class="element-input">
-                <input type="submit" value="Login">
-            </div>
-        </div>
-    </form>
+    </div>
+    <div class="col-3"></div>
 </div>
+
+
+
+
+
+<!--<div class="login-form">-->
+<!--    <form action="login.php">-->
+<!--        <div class="login-form-element">-->
+<!--            Welcome to the order taking platform-->
+<!--        </div>-->
+<!--        <div class="login-form-element">-->
+<!--            <div class="element-title">-->
+<!--                Username-->
+<!--            </div>-->
+<!--            <div class="element-input">-->
+<!--                <input name="username" type="text">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="login-form-element">-->
+<!---->
+<!--            <div class="element-title">-->
+<!--                Password-->
+<!--            </div>-->
+<!--            <div class="element-input">-->
+<!--                <input name="password" type="password">-->
+<!--            </div>-->
+<!---->
+<!--        </div>-->
+<!--        <div class="login-form-element submit">-->
+<!--            <div class="element-input">-->
+<!--                <input type="submit" value="Login">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </form>-->
+<!--</div>-->
 
 
 <?php
