b7313bc989e7c9b95bd394d6a5d7d4b5d0072c0c
Matican Matican <maticangroup@gmail.com>
2019-11-21 20:13:54 +0330

    Amirhossein

--- orders-list.php
+++ orders-list.php
@@ -7,228 +7,65 @@ if ($_SERVER['REQUEST_URI'] !== '/login.php'):
 endif;
 ?>
 <?php require "head.php";
-get_all('orders');
+
+/**
+ * @var $orders Order[]
+ */
+$orders = get_all('orders');
 ?>
 
-    <div class="page-header">
-        <h1>Orders List</h1>
-        <a href="#" class="btn btn-create">Add new</a>
-    </div>
-    <div class="orders">
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="panel">
+                <div class="records--header">
+                    <div class="title fa-shopping-bag">
+                        <h3 class="h3">All Orders</h3>
+                        <p>Found Total <?= count($orders); ?> Orders</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
-            </div>
-        </div>
-        <div class="order">
-            <div class="order-serial">
-                123123
-            </div>
-            <div class="order-date">
-                12-12-12
-            </div>
-            <div class="order-taker">
-                Hossein Azimi
-            </div>
-            <div class="order-product-count">
-                12
-            </div>
-            <div class="order-options">
-                <a class="btn order-edit" href="#">Edit</a>
-                <a class="btn order-view" href="#">View</a>
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="panel">
+                <div class="records--list" data-title="Order Listing">
+                    <table id="recordsListView">
+                        <thead>
+                        <tr>
+                            <th>Order Create Date</th>
+                            <th>Order Taker</th>
+                            <th>Order Customer Name</th>
+                            <th>Amount</th>
+                            <th class="not-sortable">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        $totalPrice = 0;
+                        foreach ($orders as $order): ?>
+                            <tr>
+                                <td><?= $order->getCreateDate(); ?></td>
+                                <td><?= $order->getTaker(); ?></td>
+                                <td><?= $order->getCustomer()->getName() . " " . $order->getCustomer()->getFamily(); ?></td>
+                                <?php
+                                /**
+                                 * @var $items OrderItem[]
+                                 */
+                                $items = $order->getOrderItems();
+                                foreach ($items as $item): ?>
+                                    <?php $totalPrice += (str_replace(",", "", $item->getPrice()) * $item->getCount()) ?>
+                                    <td><?= $totalPrice; ?></td>
+                                <?php endforeach; ?>
+                                <td>
+                                    <a href="single-order.php?cid=<?= $order->getSerial() ?>"
+                                       class="btn btn-rounded btn-info">View</a>
+                                </td>
+
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
